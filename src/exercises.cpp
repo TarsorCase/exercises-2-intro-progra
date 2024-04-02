52111ff3d1adb80ab84e6b7bbfec518af5b2edfd
Tars <97172661+TarsorCase@users.noreply.github.com>
2024-04-02 13:34:32 -0400

    EJercicio 3 correciones

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -41,7 +41,7 @@ void exercise_2(string s1) {
 
 void exercise_3(string s1) {
   int size = s1.size();
-  if(size < 20){
+  if(size <= 20){
         for(char num : s1){
             if(num == '1'){
                 cout << "Om-nom-nom :P\n";
